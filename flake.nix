d461a9b71f25bb31c45f4871e9655213981c4168
Eddio0141 <eddio0141@gmail.com>
2023-10-14 23:36:39 +0100

    fixed to return attr

--- flake.nix
+++ flake.nix
@@ -2,14 +2,16 @@
   description = "Template for devenv flake with fix";
 
   outputs = { self, nixpkgs, pre-commit-hooks, nix, ... }@inputs:
-    templates.default = {
-      path = ./templates/simple;
-      description = "A direnv supported Nix flake with devenv integration.";
-      welcomeText = ''
-        # `.devenv` should be added to `.gitignore`
-        ```sh
-          echo .devenv >> .gitignore
-        ```
-      '';
+    {
+      templates.default = {
+        path = ./templates/simple;
+        description = "A direnv supported Nix flake with devenv integration.";
+        welcomeText = ''
+          # `.devenv` should be added to `.gitignore`
+          ```sh
+            echo .devenv >> .gitignore
+          ```
+        '';
+      };
     };
 }
