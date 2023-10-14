daccbc893fcb3b064534f263c7f340ba1f62a999
Eddio0141 <eddio0141@gmail.com>
2023-10-14 23:33:11 +0100

    added simple template

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "Template for devenv flake with fix";
+
+  outputs = { self, nixpkgs, pre-commit-hooks, nix, ... }@inputs:
+    templates.default = {
+      path = ./templates/simple;
+      description = "A direnv supported Nix flake with devenv integration.";
+      welcomeText = ''
+        # `.devenv` should be added to `.gitignore`
+        ```sh
+          echo .devenv >> .gitignore
+        ```
+      '';
+    };
+}
